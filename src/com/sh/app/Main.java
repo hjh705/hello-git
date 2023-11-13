6a607357e767221e2d842ea5c2e0523fe65b6946
hjh705 <ok9175@gmail.com>
2023-11-13 11:12:00 +0900

    Monkey bananaThrow 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -2,6 +2,7 @@ package com.sh.app;
 
 import com.sh.animal.Cat;
 import com.sh.animal.Dog;
+import com.sh.animal.Monkey;
 
 public class Main {
 
@@ -11,6 +12,7 @@ public class Main {
 		
 		new Dog().bark();
 		new Cat().jump();
+		new Monkey().bananaThrow();
 	}
 
 }
