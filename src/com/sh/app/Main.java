a2b21637c55a0b24450b466d5ef85606437445d7
hjh705 <ok9175@gmail.com>
2023-11-13 11:22:07 +0900

    Main.main 대문자로 수정

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -8,8 +8,8 @@ import com.sh.animal.Monkey;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Hello Git");
+		System.out.println("HELLO WORLD");//ctrl shift x 대문자 변환 
+		System.out.println("HELLO GIT");
 		
 		new Dog().bark();
 		new Cat().jump();
